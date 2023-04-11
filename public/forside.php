7ce096704f7aedcfcc041a7c17d9b1e0c1dcb9ab
Emil Gaarde <gaardee@gmail.com>
2023-04-11 23:02:03 +0200

    Add new aktiviteter icons

--- public/forside.php
+++ public/forside.php
@@ -60,65 +60,7 @@
 
 <section id="aktiv_forside">
   <h1>Vores aktiviteter</h1>
-  <div id="ikoner">
-    <div class="et">
-      <a href="/aktiviteter/fangerne">
-        <figure>
-          <div class="fangerne" class="ikon"></div>
-          <figcaption>
-            Fangerne<br />
-            på fortet
-          </figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/tvekamp">
-        <figure>
-          <div class="tvekamp" class="ikon"></div>
-          <figcaption>Tvekamp</figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/syvkamp
-                ">
-        <figure>
-          <div class="syv" class="ikon"></div>
-          <figcaption>7-kamp</figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/stratego">
-        <figure>
-          <div class="stratego" class="ikon"></div>
-          <figcaption>Stratego</figcaption>
-        </figure>
-      </a>
-    </div>
-
-    <div class="to">
-      <a href="/aktiviteter/race">
-        <figure>
-          <div class="race" class="ikon"></div>
-          <figcaption>Adventure Race</figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/tikamp">
-        <figure>
-          <div class="tikamp" class="ikon"></div>
-          <figcaption>10-kamp</figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/casino">
-        <figure>
-          <div class="casino" class="ikon"></div>
-          <figcaption>Casino</figcaption>
-        </figure>
-      </a>
-      <a href="/aktiviteter/medarbejder">
-        <figure>
-          <div class="medarbejder" class="ikon"></div>
-          <figcaption>Årets medarbejder</figcaption>
-        </figure>
-      </a>
-    </div>
-  </div>
+  <?php include('partials/aktiviteter.php'); ?>
 </section>
 
 <section id="billedemtekst_forside">
---
+++ public/partials/aktiviteter.php
@@ -0,0 +1,84 @@
+<div id="ikoner">
+    <div class="et">
+        <a href="/aktiviteter/fangerne">
+            <figure>
+                <div class="ikon fangerne">
+                </div>
+                <figcaption>Fangerne på fortet</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/tvekamp">
+            <figure>
+                <div class="ikon tvekamp"></div>
+                <figcaption>Tvekamp</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/syvkamp">
+            <figure>
+                <div class="ikon syv">
+                </div>
+                <figcaption>7-kamp</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/stratego">
+            <figure>
+                <div class="ikon stratego"></div>
+                <figcaption>Stratego</figcaption>
+            </figure>
+        </a>
+    </div>
+
+    <div class="to">
+        <a href="/aktiviteter/race">
+            <figure>
+                <div class="ikon race"></div>
+                <figcaption>Adventure Race</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/tikamp">
+            <figure>
+                <div class="ikon tikamp"></div>
+                <figcaption>10-kamp</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/casino">
+            <figure>
+                <div class="ikon casino"></div>
+                <figcaption>Casino</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/medarbejder">
+            <figure>
+                <div class="ikon medarbejder"></div>
+                <figcaption>Årets medarbejder</figcaption>
+            </figure>
+        </a>
+    </div>
+
+    <div class="tre">
+        <a href="/aktiviteter/fangerne-blaa-mandag">
+            <figure>
+                <div class="ikon fangerne-blaa-mandag"></div>
+                <figcaption>Fangerne på fortet - Blå mandag</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/fangerne-light">
+            <figure>
+                <div class="ikon fangerne-light"></div>
+                <figcaption>Fangerne på Fortet - Light</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/teambuilding">
+            <figure>
+                <div class="ikon teambuilding"></div>
+                <figcaption>Teambuilding</figcaption>
+            </figure>
+        </a>
+        <a href="/aktiviteter/udlejning">
+            <figure>
+                <div class="ikon udlejning"></div>
+                <figcaption>Udlejning</figcaption>
+            </figure>
+        </a>
+    </div>
+</div>
